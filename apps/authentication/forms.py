23eb2001dfafed82db2479c6286dbeb791218a54
Liu Peng <liup973@gmail.com>
2021-10-12 19:42:31 -0700

    Add reCaptcha to the login/register form

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -7,6 +7,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV3
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -15,14 +17,23 @@ class LoginForm(forms.Form):
                 "placeholder": "Username",
                 "class": "form-control"
             }
-        ))
+        )
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
                 "placeholder": "Password",
                 "class": "form-control"
             }
-        ))
+        )
+    )
+    captcha = ReCaptchaField(
+        widget=ReCaptchaV3(
+            attrs={
+                'required_score':0.85,
+            }
+        )
+    )
 
 
 class SignUpForm(UserCreationForm):
@@ -32,28 +43,39 @@ class SignUpForm(UserCreationForm):
                 "placeholder": "Username",
                 "class": "form-control"
             }
-        ))
+        )
+    )
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
                 "placeholder": "Email",
                 "class": "form-control"
             }
-        ))
+        )
+    )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
                 "placeholder": "Password",
                 "class": "form-control"
             }
-        ))
+        )
+    )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
                 "placeholder": "Password check",
                 "class": "form-control"
             }
-        ))
+        )
+    )
+    captcha = ReCaptchaField(
+        widget=ReCaptchaV3(
+            attrs={
+                'required_score':0.85,
+            }
+        )
+    )
 
     class Meta:
         model = User
