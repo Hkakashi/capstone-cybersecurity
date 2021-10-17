dd92cd8f9940059b533f24fb9461354ec617064b
Liu Peng <liup973@gmail.com>
2021-10-16 21:43:57 -0700

    Major function finished

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,18 +4,15 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path, re_path
+from django.contrib.auth import login
 from apps.home import views
 
 urlpatterns = [
 
-    # The home page
-    path('', views.index, name='home'),
-
     path('message/', views.InboxListView.as_view(), name='inbox'),
     path('message/sent/', views.SentListView.as_view(), name='sent'),
     path('message/send/', views.MessageCreateView.as_view(), name='message'),
-    
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
-
+    path('message/delete/<int:pk>/', views.MessageDeleteView.as_view(), name='delete'),
+    path('dbdump/', views.dump_database, name='dump'),
+    path('', views.profile_view, name='profile'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -4,15 +4,16 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from base64 import b64encode, b64decode
+from io import StringIO
 
-from django import template
-from django.contrib.auth import models
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import loader
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
+from django.core.management import call_command
+from django.contrib.auth.mixins import LoginRequiredMixin
 from Crypto.Random import get_random_bytes
 
 from apps.home.models import Message
@@ -20,42 +21,18 @@ from apps.home.helper import encrypt, decrypt
 
 
 @login_required(login_url="/login/")
-def index(request):
-    context = {'segment': 'index'}
+def profile_view(request):
+    context = {}
+    context['segment'] = 'profile'
+    context['message_count'] = Message.objects.filter(receiver=request.user).count()
 
-    html_template = loader.get_template('home/index.html')
+    html_template = loader.get_template('home/profile.html')
     return HttpResponse(html_template.render(context, request))
 
+class InboxListView(LoginRequiredMixin, ListView):
 
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
-
-        load_template = request.path.split('/')[-1]
-
-        if load_template == 'admin':
-            return HttpResponseRedirect(reverse('admin:index'))
-        context['segment'] = load_template
-
-        html_template = loader.get_template('home/' + load_template)
-        return HttpResponse(html_template.render(context, request))
-
-    except template.TemplateDoesNotExist:
-
-        html_template = loader.get_template('home/page-404.html')
-        return HttpResponse(html_template.render(context, request))
-
-    except:
-        html_template = loader.get_template('home/page-500.html')
-        return HttpResponse(html_template.render(context, request))
-
-class InboxListView(ListView):
-
+    login_url = '/login/'
     model = Message
-    paginate_by = 10  # if pagination is desired
 
     def get_queryset(self):
         return Message.objects.filter(receiver=self.request.user)
@@ -66,12 +43,13 @@ class InboxListView(ListView):
             message.subject = decrypt(message.subject, message.iv)
             message.content = decrypt(message.content, message.iv)
         context['segment'] = 'inbox'
+        context['message_count'] = Message.objects.filter(receiver=self.request.user).count()
         return context
 
-class SentListView(ListView):
+class SentListView(LoginRequiredMixin, ListView):
 
+    login_url = '/login/'
     model = Message
-    paginate_by = 10  # if pagination is desired
 
     def get_queryset(self):
         return Message.objects.filter(sender=self.request.user)
@@ -82,10 +60,12 @@ class SentListView(ListView):
             message.subject = decrypt(message.subject, message.iv)
             message.content = decrypt(message.content, message.iv)
         context['segment'] = 'sent'
+        context['message_count'] = Message.objects.filter(receiver=self.request.user).count()
         return context
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
 
+    login_url = '/login/'
     model = Message
     fields = ['receiver', 'subject', 'content']
     success_url = '/message/'
@@ -103,4 +83,26 @@ class MessageCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['segment'] = 'message'
+        context['message_sent'] = Message.objects.filter(sender=self.request.user).count()
+        context['message_received'] = Message.objects.filter(receiver=self.request.user).count()
+        context['message_count'] = Message.objects.filter(receiver=self.request.user).count()
         return context
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = '/login/'
+    model = Message
+    success_url = reverse_lazy('inbox')
+
+    # bypass built-in confirmation
+    # confirmation process was implemented in frontend
+    def get(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
+
+@login_required(login_url="/login/")
+def dump_database(request):
+    out = StringIO()
+    call_command('dumpdata', 'auth.user', 'home.message', indent=4, stdout=out)
+    response = HttpResponse(out.getvalue())
+    response['Content-Type'] = 'text/plain'
+    response['Content-Disposition'] = 'attachment; filename=Database.txt'
+    return response
